83fd327e516d4f2df81432adbe643cbe85f8d242
heitorcampos2 <heitormtb12@gmail.com>
2019-04-09 20:00:12 -0300

    v2

--- src/FuncionarioV1.java
+++ src/FuncionarioV1.java
@@ -1,3 +1,17 @@
+//sistema de gerenciamento de funcionarios do banco
 public class FuncionarioV1 {
+    double salario;
+    String nome;
+    String departamento;
+    String datadeentrada;
+    String rg;
+    boolean ativo;
     
+    //    aumento q o funcionario irá receber
+    void bonifica(double aumento){
+        
+    }
+    void demite(){
+        
+    }    
 }
