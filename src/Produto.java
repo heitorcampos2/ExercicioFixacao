eac2e4c3f17311d4894b91a3c7b884a59397b102
Heitor Campos <48737484+heitorcampos2@users.noreply.github.com>
2019-04-29 11:17:42 -0300

    Add files via upload

--- src/Produto.java
+++ src/Produto.java
@@ -4,11 +4,13 @@ public class Produto {
     double preco;
     
     void diminuir10(){
-        preco = preco - (preco * 0.10);
+       preco = preco - (preco * 0.10);
+       System.out.println("\n>>>> EVENTO: Desconto de 10%\n");       
     }
     
     void aumenta25(){
       preco = preco * 1.25;
+      System.out.println("\n>>>> EVENTO: Aumento de 25%\n");
     }
     
 }
